986e208ff910a266c13de4d53fd316bd3e96197e
Janisz11 <k.janiszewski@zlotoryja.xyz>
2026-08-11 17:50:14 +0200

    add security headers, externalise GA4 snippet

---
+++ web/public/gtag-init.js
@@ -0,0 +1,8 @@
+// Kept out of the HTML so the CSP can forbid inline scripts entirely.
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+window.gtag = gtag;
+gtag("js", new Date());
+gtag("config", "G-YS5KWKL9C2");
